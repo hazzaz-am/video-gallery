59348086558059df9d629c02f04acd2f76935f11
hazzaz-am <hazzazabdulmannan@gmail.com>
2024-06-11 15:06:39 +0600

    get related videos from server based on title

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -12,7 +12,15 @@ export const apiSlice = createApi({
 		getSingleVideo: builder.query({
 			query: (videoId) => `/videos/${videoId}`,
 		}),
+		getRelatedVideos: builder.query({
+			query: ({ id, title }) => {
+				const tags = title.split(" ");
+				const searchString = tags.map((tag) => `title_like=${tag}`);
+				const queryString = `/videos/?${searchString.join("&")}&_limit=4`;
+				return queryString;
+			},
+		}),
 	}),
 });
 
-export const { useGetVideosQuery, useGetSingleVideoQuery } = apiSlice;
+export const { useGetVideosQuery, useGetSingleVideoQuery, useGetRelatedVideosQuery } = apiSlice;
